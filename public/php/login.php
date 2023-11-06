5b669b5b620c9239b81cbe66c08a26a1a7ef265a
Gabriela <ariettigabriela@gmail.com>
2023-11-06 20:32:48 -0300

    login admin

--- public/php/login.php
+++ public/php/login.php
@@ -11,9 +11,14 @@ $consulta = "SELECT * FROM users WHERE mail='$mail'";
 $resultado = mysqli_query($conexion, $consulta);
 
 if (mysqli_num_rows($resultado) == 1) {
-    $verificar = mysqli_fetch_assoc ($resultado);
+    $verificar = mysqli_fetch_assoc($resultado);
     if ($verificar['contra'] == $pass) {
         header("location: ../views/main.html");
+        if ($verificar['permiso'] == 0) {
+            header("location: ../views/main.html");
+        } else {
+            header("location: ../views/admin.html");
+        }
     } else {
         header("location:../../index.html?error=1");
     }
